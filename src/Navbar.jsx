834db31acf1f67e9c404311211bd8766f23d63cb
Diana Chong <dchong0123@gmail.com>
2023-02-17 02:57:13 -0500

    completed Projects & Resume sections and implemented styling for desktop version

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Resume from './components/Resume.pdf';
+import Resume from './components/DianaChongResume.pdf';
 
 const Navbar = () => {
   return(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,16 @@ import { WindupChildren, Pause, Pace } from 'windups';
 import Navbar from '../Navbar.jsx';
 import Footer from '../components/Footer.jsx';
 import me from '../components/images/me.jpeg';
+import background from '../components/images/green-top.png';
+import background2 from '../components/images/green-bottom.png';
 
 const Home = () => {
   return(
     <div className='center'>
-      <div>
+      <div style={{backgroundSize: 'contain', backgroundRepeat: 'repeat-x', backgroundImage: `url(${background})`}}>
         <img className='me' src={me} />
-      </div>
-      <h1>Diana Chong</h1>
-      <Navbar />
-      <div className='bounded'>
+        <h1 className='name'>Diana Chong</h1>
+        <Navbar />
         <h1 className='windup'>
           <WindupChildren>
             <Pace ms={100}>
@@ -25,52 +25,56 @@ const Home = () => {
             </Pace>
           </WindupChildren>
         </h1>
-        <div className='skill-section'>
-          <h4 className='skillTitle'>Front End</h4>
-          <span className='skill'>JavaScript</span>
-          <span className='skill'>HTML</span>
-          <span className='skill'>CSS</span>
-          <span className='skill'>React</span>
-        </div>
-        <div className='skill-section'>
-          <h4 className='skillTitle'>Back End</h4>
-          <span className='skill'>Node.JS</span>
-          <span className='skill'>Express.JS</span>
-          <span className='skill'>SQL</span>
-          <span className='skill'>PostgreSQL</span>
-          <span className='skill'>MySQL</span>
-          <span className='skill'>MongoDB</span>
-          <span className='skill'>Mongoose</span>
-          <span className='skill'>Restful API Development</span>
-        </div>
-        <div className='skill-section'>
-          <h4 className='skillTitle'>Testing & Deployment</h4>
-          <span className='skill'>Jest</span>
-          <span className='skill'>Mocha</span>
-          <span className='skill'>Chai</span>
-          <span className='skill'>AWS EC2</span>
-          <span className='skill'>K6</span>
-          <span className='skill'>Loader.io</span>
-        </div>
-        <div className='skill-section'>
-          <h4 className='skillTitle'>Developer Tools</h4>
-          <span className='skill'>Git</span>
-          <span className='skill'>npm</span>
-          <span className='skill'>Webpack</span>
-          <span className='skill'>Babel</span>
-          <span className='skill'>Postman</span>
-          <span className='skill'>Agile Methodology</span>
-          <span className='skill'>Scrum</span>
-          <span className='skill'>JIRA</span>
-          <span className='skill'>Test Driven Development (TDD)</span>
-        </div>
-        <div className='skill-section'>
-          <h4 className='skillTitle'>Certifications & Other</h4>
-          <span className='skill'>CompTIA Security+</span>
-          <span className='skill'>AWS Certified Cloud Practitioner</span>
-        </div>
       </div>
-      <Footer />
+      <div style={{backgroundSize: 'contain', backgroundRepeat: 'repeat-x', backgroundPosition: 'bottom', backgroundImage: `url(${background2})`}}>
+        <div className='bounded'>
+          <div className='skill-section'>
+            <h4 className='skillTitle'>Front End</h4>
+            <span className='skill'>JavaScript</span>
+            <span className='skill'>HTML</span>
+            <span className='skill'>CSS</span>
+            <span className='skill'>React</span>
+          </div>
+          <div className='skill-section'>
+            <h4 className='skillTitle'>Back End</h4>
+            <span className='skill'>Node.JS</span>
+            <span className='skill'>Express.JS</span>
+            <span className='skill'>SQL</span>
+            <span className='skill'>PostgreSQL</span>
+            <span className='skill'>MySQL</span>
+            <span className='skill'>MongoDB</span>
+            <span className='skill'>Mongoose</span>
+            <span className='skill'>Restful API Development</span>
+          </div>
+          <div className='skill-section'>
+            <h4 className='skillTitle'>Testing & Deployment</h4>
+            <span className='skill'>Jest</span>
+            <span className='skill'>Mocha</span>
+            <span className='skill'>Chai</span>
+            <span className='skill'>AWS EC2</span>
+            <span className='skill'>K6</span>
+            <span className='skill'>Loader.io</span>
+          </div>
+          <div className='skill-section'>
+            <h4 className='skillTitle'>Developer Tools</h4>
+            <span className='skill'>Git</span>
+            <span className='skill'>npm</span>
+            <span className='skill'>Webpack</span>
+            <span className='skill'>Babel</span>
+            <span className='skill'>Postman</span>
+            <span className='skill'>Agile Methodology</span>
+            <span className='skill'>Scrum</span>
+            <span className='skill'>JIRA</span>
+            <span className='skill'>Test Driven Development (TDD)</span>
+          </div>
+          <div className='skill-section'>
+            <h4 className='skillTitle'>Certifications & Other</h4>
+            <span className='skill'>CompTIA Security+</span>
+            <span className='skill'>AWS Certified Cloud Practitioner</span>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   )
 }
